d5e3846caf4d1b7acd00d28f01ff5c0bab00af04
Nicolas Claudon <nicolasclaudon@gmail.com>
2011-11-15 18:23:04 +0100

    Add proper factories for user model, transportation model, and destination model

--- spec/factories.rb
+++ spec/factories.rb
@@ -6,3 +6,12 @@ Factory.define :user do |u|
   u.password 'please'
 end
 
+Factory.define :transportation do |t|
+  t.mode 'Bus'
+end
+
+Factory.define :destination do |d|
+  d.name 'Destination test'
+  d.arrival Date.today
+  d.departure Date.today + 1.day
+end
